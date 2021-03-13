895eb1e824c6d9c1e0c4fe39353cb37c27fc1be2
deusart <dmitry.tavakalov@gmail.com>
2021-03-13 23:41:25 +0300

    settings template

---
+++ config/settings_template.py
@@ -0,0 +1,13 @@
+# Catalog
+# in config folder there is should be settings.py file based on this template
+FOLDER = 'json repository path'
+LOGIN = 'username'
+PASSWORD = 'password'
+ENCODING = 'UTF-16'
+HEADER = {'Accept': 'text/plain', 'Content-Type': 'application/json'}
+CREDENTIAL = {'login': LOGIN, 'password': PASSWORD}
+LIMIT = 10000
+OUTPUT = print
+UPDATE_DAYS = 1
+
+
