46fd82257e87f277099e331b899bceb86c2ba6ae
deusart <dmitry.tavakalov@gmail.com>
2021-03-22 00:27:26 +0300

    refactoring

--- catalogapp/__init__.py
+++ catalogapp/__init__.py
@@ -1,14 +1,1 @@
 import catalogapp.assembly as v1
-
-
-        # if catalog_type == 'dictionaries':
-        #     self._set_dictionaries()
-        # elif catalog_type == 'models':
-        #     self._set_models()
-        # elif 'prices' in catalog_type:
-        #     self._set_prices(catalog_type)
-        # elif catalog_type == 'modified':
-        #     self._set_modified()
-        # elif catalog_type == 'categories-details':
-        #     self._set_categories_details()
-        # print('')
--- catalogapp/cappengine/utils/files.py
+++ catalogapp/cappengine/utils/files.py
@@ -2,14 +2,6 @@ import json
 import datetime
 import os
 
-def save_extract_state(path, data, name='extract_state', encoding='UTF-16'):
-    try:
-        with open(f'{path}{name}.json', 'w', encoding=encoding) as outfile:
-            outfile.write(json.dumps(data, ensure_ascii=False))
-        return f'File {path}{name}.json is saved.'
-    except Exception as error:
-        return error
-
 def save_json(path, entity, data, encoding='UTF-16', timestamp = False):
     if timestamp:
         entity = f'{entity}_{int(datetime.datetime.now().timestamp())}'
@@ -20,7 +12,7 @@ def save_json(path, entity, data, encoding='UTF-16', timestamp = False):
     except Exception as error:
         return error
         
-def directory_exists(path, create=True):    
+def directory_exists(path):    
     if not os.path.exists(path):
         try:
             os.mkdir(path)
